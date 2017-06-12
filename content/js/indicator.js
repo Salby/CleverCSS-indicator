5bcae1336abd64b7e4a2787a524a3769c20e2043
Sherlock <SanderSalamander@gmail.com>
2017-06-12 12:42:14 +0200

    Added untracked files to commit

---
+++ content/js/indicator.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  var indicatorPos_start = $('.cat-active').offset().left;
+  $('.cat-indicator').css('left', indicatorPos_start);
+
+  $('.button-cat').click(function() {
+    $(this).siblings().removeClass('cat-active')
+    $(this).addClass('cat-active')
+
+    var indicatorPos = $(this).offset().left;
+    $('.cat-indicator').css('left', indicatorPos);
+
+    //|||||||||||||||||||||||||||||||||||||||||//
+    // SECTION MANAGEMENT -DO NOT INCLUDE-
+    var target = $(this).data('target');
+    $(target).siblings().addClass('cat-hide');
+    $(target).removeClass('cat-hide');
+    //|||||||||||||||||||||||||||||||||||||||||//
+  });
+
+});
